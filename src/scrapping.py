f681ab1691cafb7b951b92b6672f1d2aee0fe828
Marcus Blake <mab2401@columbia.edu>
2018-08-02 05:09:51 +0000

    Changed setup.sh to an executable and added to scrapping.py

--- src/scrapping.py
+++ src/scrapping.py
@@ -7,8 +7,18 @@ soup = BeautifulSoup(req.text, 'html.parser')
 
 
 def run():
-    print soup.prettify()
+
+	#Getting necessary labels for the data
+	headers = soup.findAll('tr', class_='thead')
+	part1 = headers[0]
+	part2 = headers[1]
+
+	#get tables
+	tables = soup.findAll('tr', class_='full_table')
+	
+	print tables
+    
 
 
 if __name__=="__main__":
-    run()
+	run()
