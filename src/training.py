e6ef07ec57b27f4a404f75ca555115ee8d9485f6
Marcus Blake <mab2401@columbia.edu>
2018-08-11 19:52:58 +0000

    Added line to get data set using Pandas

--- src/training.py
+++ src/training.py
@@ -9,9 +9,8 @@ from scraping import currentRookieData
 import pandas as pd
 
 def main():
-	rookies_of_year = pd.Series( ROYData() )
-	print rookies_of_year
-
+	nba_historical_data = pd.read_excel('https://query.data.world/s/ntr4fv2oniqbrs4b55epcyyia5x66x')
+	print nba_historical_data
 
 if __name__ == "__main__":
 	main() 
