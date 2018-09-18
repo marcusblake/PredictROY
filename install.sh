63ce462f530524d6063dec4156e4ea1ce059a405
marcusblake <32970144+marcusblake@users.noreply.github.com>
2018-09-18 16:55:42 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,3 @@
 
 python -mpip install matplotlib numpy scipy
 pip install beautifulsoup4 pandas requests xlrd scikit-learn
-pip install --upgrade https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.10.0-py2-none-any.whl
